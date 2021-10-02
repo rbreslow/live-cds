90797e95e12705c8961bc79076cf6ffe09dff8ba
Rocky Breslow <breslowrocky@gmail.com>
2021-10-01 21:44:39 -0400

    Enable pcscd which is required by yubikey-manager

--- i686-gnupg/flake.nix
+++ i686-gnupg/flake.nix
@@ -27,14 +27,16 @@
             boot.kernelParams = [ "forcepae" ];
 
             environment.systemPackages = [ 
+              # https://www.jabberwocky.com/software/paperkey/
+              pkgs.paperkey
               # TODO: What's the difference between yubikey-manager and
               # yubikey-personalization? I'm only installing the latter due to
               # this reccomendation: https://nixos.wiki/wiki/Yubikey.
               pkgs.yubikey-manager
-              # https://www.jabberwocky.com/software/paperkey/
-              pkgs.paperkey 
             ];
 
+            # https://ludovicrousseau.blogspot.com/2019/06/gnupg-and-pcsc-conflicts.html
+            services.pcscd.enable = true;
             services.udev.packages = [ pkgs.yubikey-personalization ];
           })
         ];
